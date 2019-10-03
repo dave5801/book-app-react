4d374629f63b55c15289d6e7bcb4a97fea4d020a
David <dave5801@gmail.com>
2019-10-03 14:32:22 -0700

    experimenting with tables on body

--- book-app/src/Body.js
+++ book-app/src/Body.js
@@ -1,5 +1,41 @@
 import React from 'react';
+
+class TableRow extends React.Component {
+    render() {
+       return (
+          <tr>
+             <td>{this.props.data.id}</td>
+             <td>{this.props.data.name}</td>
+             <td>{this.props.data.age}</td>
+          </tr>
+       );
+    }
+ }
+
 class Body extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+           data: 
+           [
+              {
+                 "id":1,
+                 "name":"Foo",
+                 "age":"20"
+              },
+              {
+                 "id":2,
+                 "name":"Bar",
+                 "age":"30"
+              },
+              {
+                 "id":3,
+                 "name":"Baz",
+                 "age":"40"
+              }
+           ]
+        }
+     }
     render(){
       return (
         <div className="App">
@@ -8,6 +44,12 @@ class Body extends React.Component{
             <p>
               This is my book application
             </p>
+            <table>
+               <tbody>
+                  {this.state.data.map((person, i) => <TableRow key = {i} 
+                     data = {person} />)}
+               </tbody>
+            </table>
             <a
               className="App-link"
               href="https://reactjs.org"
