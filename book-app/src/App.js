16375eb96b3facdbd7985e04ba33818c41851c2d
David <dave5801@gmail.com>
2019-10-03 13:51:22 -0700

    moved body component to seperate file

--- book-app/src/App.js
+++ book-app/src/App.js
@@ -1,50 +1,11 @@
 import React from 'react';
+import Body from './Body';
 import './App.css';
-
-class Body extends React.Component{
-  render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-  
-          <p>
-            This is my book application
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            This will be the wishlist
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
 
 class App extends React.Component{
   render(){
     return (
       <Body/>
-      /*
-      <div className="App">
-        <header className="App-header">
-  
-          <p>
-            This is my book application
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            This will be the wishlist
-          </a>
-        </header>
-      </div>*/
     );
   }
 }
---
+++ book-app/src/Body.js
@@ -0,0 +1,24 @@
+import React from 'react';
+class Body extends React.Component{
+    render(){
+      return (
+        <div className="App">
+          <header className="App-header">
+    
+            <p>
+              This is my book application
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              This will be the wishlist
+            </a>
+          </header>
+        </div>
+      );
+    }
+  }
+  export default Body;
