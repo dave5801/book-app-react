5cb025c48c4678f583c0f0745cf02448fce1914e
David <dave5801@gmail.com>
2019-10-03 13:37:28 -0700

    making new components

--- book-app/src/App.js
+++ book-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-class App extends React.Component{
+class Body extends React.Component{
   render(){
     return (
       <div className="App">
@@ -20,6 +20,31 @@ class App extends React.Component{
           </a>
         </header>
       </div>
+    );
+  }
+}
+
+class App extends React.Component{
+  render(){
+    return (
+      <Body/>
+      /*
+      <div className="App">
+        <header className="App-header">
+  
+          <p>
+            This is my book application
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            This will be the wishlist
+          </a>
+        </header>
+      </div>*/
     );
   }
 }
