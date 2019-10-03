e73062265186e7b7532c72f98f6c70f30d57707b
David <dave5801@gmail.com>
2019-10-03 13:22:20 -0700

    reformat App main

--- book-app/src/App.js
+++ book-app/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-//import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-
-        <p>
-          This is my book application
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          This will be the wishlist
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+  
+          <p>
+            This is my book application
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            This will be the wishlist
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
